789a81185978feaf052dbeb412a0c315dbc3fdd5
GM-Liquid <matveisushko@gmail.com>
2024-12-19 00:10:11 +0200

    v0001

--- module/myrpg.js
+++ module/myrpg.js
@@ -1,11 +1,25 @@
 console.log("My RPG System | Загрузка системы");
 
+
+import { Character } from "templates/actors/actor-character.json";
+import { Items } from "templates/actors/items.json";
+
+
+Hooks.once('init', function () {
+    // Add utility classes to the global game object so that they're more easily
+    // accessible in global contexts.
+    game.boilerplate = {
+        BoilerplateActor,
+        BoilerplateItem,
+        rollItemMacro,
+    };
+
 class MyRPGActor extends Actor {
     prepareData() {
         super.prepareData();
         const actorData = this.system;
 
-        // Инициализируем базовую структуру, если её нет
+        // Создаем базовые аттрибуты
         if (!actorData.attributes) {
             actorData.attributes = {
                 body: { value: 10, label: "Телосложение" },
