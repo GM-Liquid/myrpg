e04b125d98c15c7a533c4d327c147c0ab7d49c56
GM-Liquid <matveisushko@gmail.com>
2024-12-19 15:02:33 +0200

    v0.5

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const MY_SHEET = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+MY_SHEET.abilities = {
+  str: 'MY_SHEET.Ability.Str.long',
+  dex: 'MY_SHEET.Ability.Dex.long',
+  con: 'MY_SHEET.Ability.Con.long',
+  int: 'MY_SHEET.Ability.Int.long',
+  wis: 'MY_SHEET.Ability.Wis.long',
+  cha: 'MY_SHEET.Ability.Cha.long',
+};
+
+MY_SHEET.abilityAbbreviations = {
+  str: 'MY_SHEET.Ability.Str.abbr',
+  dex: 'MY_SHEET.Ability.Dex.abbr',
+  con: 'MY_SHEET.Ability.Con.abbr',
+  int: 'MY_SHEET.Ability.Int.abbr',
+  wis: 'MY_SHEET.Ability.Wis.abbr',
+  cha: 'MY_SHEET.Ability.Cha.abbr',
+};
