d52e1e895a4f3db117087e7beb8028b6dbb3470c
GM-Liquid <matveisushko@gmail.com>
2025-02-27 12:33:04 +0200

    1

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -82,6 +82,14 @@ export class myrpgActorSheet extends ActorSheet {
             // Открываем отдельное окошко (FormApplication) для редактирования
             new MyAbilityConfig(this.actor, index, abilityData).render(true);
         });
+        html.find('.ability-name').each((i, el) => {
+            const tooltipContent = $(el).find('.ability-tooltip').html();
+            if (tooltipContent) {
+                $(el).attr('data-tooltip', tooltipContent);
+            }
+        });
+        // Активируем обработчики подсказок от Foundry
+        TooltipManager.activateEventListeners(html);
     }
     static get defaultOptions() {
         return foundry.utils.mergeObject(super.defaultOptions, {
