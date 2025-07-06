c4c396d0aea802d14bc24c9e97aff4cac89ed777
GM-Liquid <150024836+GM-Liquid@users.noreply.github.com>
2025-07-07 00:21:26 +0300

    fix: remove unused ability row template

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -6,8 +6,7 @@
 export const preloadHandlebarsTemplates = async function () {
   const paths = [
     'systems/myrpg/templates/actor/actor-character-sheet.hbs',
-    'systems/myrpg/templates/actor/actor-npc-sheet.hbs',
-    'systems/myrpg/templates/partials/ability-row.hbs'
+    'systems/myrpg/templates/actor/actor-npc-sheet.hbs'
   ];
   return loadTemplates(paths);
 };
