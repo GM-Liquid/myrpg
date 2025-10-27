1bc62293913750fc9cd1a321bcb431d24b97c242
GM-Liquid <150024836+GM-Liquid@users.noreply.github.com>
2025-10-27 12:41:33 +0200

    feat: add item documents and sheets

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -6,7 +6,12 @@
 export const preloadHandlebarsTemplates = async function () {
   const paths = [
     'systems/myrpg/templates/actor/actor-character-sheet.hbs',
-    'systems/myrpg/templates/actor/actor-npc-sheet.hbs'
+    'systems/myrpg/templates/actor/actor-npc-sheet.hbs',
+    'systems/myrpg/templates/item/ability-sheet.hbs',
+    'systems/myrpg/templates/item/mod-sheet.hbs',
+    'systems/myrpg/templates/item/armor-sheet.hbs',
+    'systems/myrpg/templates/item/weapon-sheet.hbs',
+    'systems/myrpg/templates/item/gear-sheet.hbs'
   ];
   return loadTemplates(paths);
 };
